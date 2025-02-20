d8e35b6e80d920f60b5a7a2963bc096f94db9bca
a001985 <magnus.wenzer@smhi.se>
2025-02-20 17:27:43 +0100

    Remove nodc_codes update

--- src/sharkadm_zip_publisher/__init__.py
+++ src/sharkadm_zip_publisher/__init__.py
@@ -1,10 +1,3 @@
 from sharkadm_zip_publisher.flet_app import run_app
 
 from sharkadm import adm_logger
-
-try:
-    from nodc_codes import update_config_files
-    update_config_files()
-    adm_logger.log_workflow(f'nodc_codes config files updated successfully.', level=adm_logger.INFO)
-except Exception as e:
-    adm_logger.log_workflow(f'Could not update nodc_codes config files', level=adm_logger.WARNING)
